1521d26a04282d9593bab16e3f99928440a97bc8
nivram761 <marvinxu2004@gmail.com>
2024-10-21 16:31:59 -0400

    profile changes

--- EventExpert/users/models.py
+++ EventExpert/users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.files.storage import default_storage
 
-# Create your models here.
+# Function to provide default profile picture path
+def default_profile_pic():
+    return 'profile_pics/default_pfp.jpg'  # You should have a default image in 'media/profile_pics/'
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', default=default_profile_pic)
+
+    def __str__(self):
+        return self.user.username
+
+# Signal to create or update the UserProfile when User is created/updated
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.userprofile.save()
--- EventExpert/users/views.py
+++ EventExpert/users/views.py
@@ -6,6 +6,54 @@ from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import UserProfile  # Import the UserProfile model
+
+
+# Serializer for user details including profile fields
+class UserProfileSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField(source='user.first_name')
+    last_name = serializers.CharField(source='user.last_name')
+    email = serializers.EmailField(source='user.email')
+    username = serializers.CharField(source='user.username')
+    profile_picture = serializers.ImageField()
+
+    class Meta:
+        model = UserProfile
+        fields = ['username', 'first_name', 'last_name', 'email', 'profile_picture']
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop('user', {})
+        user = instance.user
+
+        # Update the User model fields (first_name, last_name, etc.)
+        user.first_name = user_data.get('first_name', user.first_name)
+        user.last_name = user_data.get('last_name', user.last_name)
+        user.email = user_data.get('email', user.email)
+        user.save()
+
+        # Update profile picture
+        instance.profile_picture = validated_data.get('profile_picture', instance.profile_picture)
+        instance.save()
+
+        return instance
+
+
+@api_view(['GET', 'PUT'])
+@permission_classes([IsAuthenticated])
+def user_profile_view(request):
+    user = request.user
+    profile = user.userprofile  # Access the UserProfile via OneToOne relationship
+
+    if request.method == 'GET':
+        serializer = UserProfileSerializer(profile)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = UserProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['POST'])
@@ -14,6 +62,8 @@ def register_user(request):
     username = request.data.get('username')
     email = request.data.get('email')
     password = request.data.get('password')
+    first_name = request.data.get('first_name')  # Get first name
+    last_name = request.data.get('last_name')  # Get last name
 
     if User.objects.filter(username=username).exists():
         return Response({"error": "Username already exists"}, status=status.HTTP_400_BAD_REQUEST)
@@ -21,19 +71,21 @@ def register_user(request):
     user = User.objects.create(
         username=username,
         email=email,
-        password=make_password(password)  # Ensure the password is hashed
+        password=make_password(password),
+        first_name=first_name,  # Save first name
+        last_name=last_name  # Save last name
     )
     return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
 
-# Serializer for user details
+# Serializer for basic user details
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email']
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_user_profile(request):
     user = request.user
     serializer = UserSerializer(user)
     return Response(serializer.data)
-    
